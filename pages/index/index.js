8115081b3e6831644b21f73423a3d6825c32a67a
bboylin <bboylin24@gmail.com>
2018-04-21 00:56:46 +0800

    加载更多

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,39 +7,77 @@ var page = 1;
 Page({
   data: {
     items: [],
-    hidden: false
+    hidden: false,
+    wHeight: 0
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: "../logs/logs"
-    });
+  onTitleClick: function() {
+    wx.showToast({
+      title: "个人开发者不支持webview",
+      icon: "none"
+    })
+  },
+  onImageClick: function() {
+    wx.showToast({
+      title: "image",
+      icon: "success"
+    })
+  },
+  refresh: function(){
+    page = 1;
+    console.log('下拉刷新')
+    load(this)
+  },
+  loadMore: function(){
+    console.log('加载更多')
+    load(this)
   },
   onLoad: function() {
-    wx.request({
-      url: "http://gank.io/api/data/all/10/" + page,
-      data: {},
-      header: {
-        "Content-Type": "application/json"
-      },
-      method: "GET", // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      success: res => {
-        if (
-          res == null ||
-          res.data == null ||
-          res.data.results == null ||
-          res.data.results.length <= 0
-        ) {
-          console.error('服务器数据为null');
-          return;
-        }
-        console.log(res.data);
-        this.setData({
-          items: [...this.data.items, ...res.data.results],
-          hidden: true
-        });
-        page += 1;
+    var that = this;
+    load(that)
+    wx.getSystemInfo({
+      success: function(res) {
+        console.log(res)
+        console.log(res.windowHeight)
+        that.setData({
+          wHeight: res.windowHeight
+        })
       }
-    });
+    })
   }
 });
+
+function load(that){
+  wx.request({
+    url: "http://gank.io/api/data/all/10/" + page,
+    data: {},
+    header: {
+      "Content-Type": "application/json"
+    },
+    method: "GET", // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+    success: res => {
+      if (
+        res == null ||
+        res.data == null ||
+        res.data.results == null ||
+        res.data.results.length <= 0
+      ) {
+        console.error('服务器数据为null');
+        return;
+      }
+      console.log(res.data);
+      if(page==1){
+        that.setData({
+          items: res.data.results,
+          hidden: true
+        });
+      }else{
+        that.setData({
+          items: [...that.data.items, ...res.data.results],
+          hidden: true
+        });
+      }
+      page += 1;
+    }
+  });
+}
